734f756f6cdee90a44ac3bbaf6b34a8ddbd75327
ŁUKASZ OLSZEWSKI/lolszewski/Elec.Dep Prod D2.Equip T.Equip P <lolszewski@lgchem.com>
2021-04-08 16:04:36 +0200

    Add fuction to open hamburger menu, right navigation

--- src/Modyfication/Navbar/Hamburger/Hamburger.js
+++ src/Modyfication/Navbar/Hamburger/Hamburger.js
@@ -6,14 +6,20 @@ import {FiMenu} from "react-icons/fi";
 function Hamburger() {
 
     const [clickHamburger, setClicHamburger] = React.useState(false);
+    const [showMenu,setMenu] = React.useState(false);
 
     const clickMenu =() =>{
         setClicHamburger(!clickHamburger);
+        setMenu(!showMenu);
     }
+
+    let widthValue;
+
 
     return (
         <div className="hamburger-conteiner">
             <a href="#" className="hamburger-menu-icon" onClick={clickMenu}> {clickHamburger ? <AiOutlineClose/> : <FiMenu/>}</a>
+            <div className= {showMenu ? "menu-link-box active" : "menu-link-box"}></div>
         </div>
     )
 }
