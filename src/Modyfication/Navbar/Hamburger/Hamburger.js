9337a05d30eafcbdc12b06462979f49f68b11cbf
ŁUKASZ OLSZEWSKI/lolszewski/Elec.Dep Prod D2.Equip T.Equip P <lolszewski@lgchem.com>
2021-04-16 13:18:27 +0200

    Add links in right navbar with css

--- src/Modyfication/Navbar/Hamburger/Hamburger.js
+++ src/Modyfication/Navbar/Hamburger/Hamburger.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Hamburger.css';
 import {AiOutlineClose} from "react-icons/ai";
 import {FiMenu} from "react-icons/fi";
+import {MdDashboard} from "react-icons/md";
+import {HiFolderAdd} from "react-icons/hi";
 
 function Hamburger() {
 
@@ -17,8 +19,13 @@ function Hamburger() {
 
     return (
         <div className="hamburger-conteiner">
-            <a href="#" className="hamburger-menu-icon" onClick={clickMenu}> {clickHamburger ? <AiOutlineClose/> : <FiMenu/>}</a>
-            <div className= {showMenu ? "menu-link-box active" : "menu-link-box"}></div>
+            <a href="#" className={clickHamburger ?"hamburger-menu-icon active":"hamburger-menu-icon"} onClick={clickMenu}> {clickHamburger ? <AiOutlineClose/> : <FiMenu/>}</a>
+            <div className= {showMenu ? "menu-link-box active" : "menu-link-box"}>
+                <div className="link-conteiner">
+                    <div className="link-box"><a href="/"><div className="icon-link"><MdDashboard/></div>Dashboard</a></div>
+                    <div className="link-box"><a href="/"><div className="icon-link"><HiFolderAdd/></div>Modyfication</a></div>
+                </div>
+            </div>
         </div>
     )
 }
